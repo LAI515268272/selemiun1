0f40375e00969af83311dc860ca381d3bcbc71e6
Administrator <LAI515268272>
2020-11-27 13:29:09 +0800

    122

--- src/main/java/com/lsj/word1/demo/demo1.java
+++ src/main/java/com/lsj/word1/demo/demo1.java
@@ -1,4 +1,46 @@
 package com.lsj.word1.demo;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.*;
 
+import java.util.concurrent.TimeUnit;
+
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class demo1 {
+
+    private static   WebDriver chrome;
+
+
+    @BeforeClass
+    public static void qudong(){
+
+       System.setProperty("webdriver.chrome.driver","D:\\WM\\chromedriver.exe");
+
+        chrome = new ChromeDriver();
+
+        chrome.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+
+    }
+
+    @Test
+    public void test1(){
+
+        chrome.get("http://www.baidu.com");
+
+        chrome.findElement(By.id("kw")).sendKeys("java");
+
+        chrome.findElement(By.xpath("//*[@id=\"su\"]")).click();
+
+
+
+    }
+
 }
--- src/test/java/com/lsj/word1/demo/demo1Test.java
+++ src/test/java/com/lsj/word1/demo/demo1Test.java
@@ -1,4 +1,8 @@
+package com.lsj.word1.demo;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+
 class demo1Test {
-  
+
 }
