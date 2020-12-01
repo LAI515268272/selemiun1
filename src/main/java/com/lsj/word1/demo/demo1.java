81167af0d91db032c230d6a20094f4613c3387ec
Administrator <LAI515268272>
2020-12-01 10:05:43 +0800

    修改引用类

--- src/main/java/com/lsj/word1/demo/demo1.java
+++ src/main/java/com/lsj/word1/demo/demo1.java
@@ -6,7 +6,7 @@ import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.slf4j.*;
+
 
 import java.util.concurrent.TimeUnit;
 
